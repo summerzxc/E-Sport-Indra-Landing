61e96005b6cf81f251b7c047eaca66ed10107a62
summerzxc <cell.unobtainable.88@gmail.com>
2024-04-23 13:16:29 +0800

    Credits

--- src/pages/components/Footer.jsx
+++ src/pages/components/Footer.jsx
@@ -10,6 +10,7 @@ export default function Footer() {
         <h3 className="text-xl font-[500] tracking-tight">X</h3>
         <h3 className="text-3xl font-[700] tracking-tight">INDRA</h3>
       </div>
+      <span className="text-[#888] text-center tracking-tight text-[14px]">Developed by SW-23 student Anand.B (JinDev)</span>
     </div>
   );
 }
